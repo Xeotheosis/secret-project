658bddd54c40c738a0474503a1cb0a41740b8936
Andrei Neacsu <neacsu.andrei.ro@icloud.com>
2023-07-12 00:45:03 +0300

    Update project for deployment

--- src/RandomNumberGenerator.jsx
+++ src/RandomNumberGenerator.jsx
@@ -2,18 +2,27 @@ import React, { useState } from 'react';
 
 function RandomNumberGenerator() {
   const [randomWord, setRandomWord] = useState(null);
+  const [previousWord, setPreviousWord] = useState(null);
   const [randomNumber, setRandomNumber] = useState(null);
   const [randomPresent, setRandomPresent] = useState(null);
+  const [previousPresent, setPreviousPresent] = useState(null);
 
   const presentArray = ["one catte", "I steal u", "I steal your foot", "I eat your snacks", "I kiss u","I kiss u 200 times", "I make u coffe", "I kiss your cheek"]
 
   const generateRandomPresent = () => {
-    const randomPresentIndex = Math.floor(Math.random() * presentArray.length);
-    const present = presentArray[randomPresentIndex];
-    setRandomPresent(present);
+    let newRandomPresent = presentArray[Math.floor(Math.random() * presentArray.length)];
+
+    // Generate a new present until it is different from the previous present
+    while (newRandomPresent === previousPresent) {
+      newRandomPresent = presentArray[Math.floor(Math.random() * presentArray.length)];
+    }
+
+    setPreviousPresent(randomPresent);
+    setRandomPresent(newRandomPresent);
   }
 
-  const wordArray = ["Idiot", "Abibas", "Pineapple", "Banana", "Cacahuette", "Baka Baka", "Chemchkin"];
+
+  const wordArray = ["Idiot!", "Abibas", "Pineapple", "Banana", "Cacahuette", "Baka Baka", "Chemchkin"];
 
   const generateRandomWord = () => {
     const randomIndex = Math.floor(Math.random() * wordArray.length);
@@ -32,12 +41,15 @@ function RandomNumberGenerator() {
   return (
     <div>
       <h1>Today's Stats</h1>
+      <hr/>
       <p>What Kind Of Baka Are You Today?</p>
       <button onClick={generateRandomWord}>Baka</button>
       {randomWord && <h2>Baka {randomWord}</h2>}
+      <hr/>
       <p>How Much Do I Love You Today?</p>
       <button onClick={generateRandomNumber}>Find Out</button>
       {randomNumber && <h2>{randomNumber.toLocaleString()} million (quite a lot)</h2>}
+      <hr/>
       <p>My Gift For You Today</p>
       <button onClick ={generateRandomPresent}>Surprise</button>
       {randomPresent && <h2>{randomPresent}</h2>}
